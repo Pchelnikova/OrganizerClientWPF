317eb23f88b4580662f1ad3e511a8ad8e1370cb3
Irina <pchelnikova23@gmail.com>
2018-06-30 15:10:45 +0300

    Wish-List added. (third part)

---
+++ OrganizerClientWPF/ThreeParts_Windows/WISH.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using MahApps.Metro.Controls;
+using DALOrganizerClientWPF;
+using OrganizerClientWPF.DTO;
+
+namespace OrganizerClientWPF
+{
+    /// <summary>
+    /// Interaction logic for WISH.xaml
+    /// </summary>
+    public partial class WISH : MetroWindow
+    {
+        private readonly DataDAL _dalCl = new DataDAL();
+        public User CurrentUser { get; } = new User();
+
+        public WISH (User currentUser)
+        {
+            InitializeComponent();
+            CurrentUser = currentUser;
+            //title.Text = CurrentUser.Login.ToString().ToUpper() + "'s Wish-List";
+
+        }
+    }
+}
